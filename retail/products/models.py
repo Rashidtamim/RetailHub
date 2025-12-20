45e6c1779af77b752345d9db5e8cfd02ff93655d
Rashid <rashidtamim999@gmail.com>
2025-12-21 02:37:47 +0600

    3rd_commit

--- retail/products/models.py
+++ retail/products/models.py
@@ -10,28 +10,10 @@ class Category(BaseModel):
     category_image = models.ImageField(upload_to="catgories")
 
 
-    def save(self , *args , **kwargs):
-        self.slug = slugify(self.category_name)
-        super(Category ,self).save(*args , **kwargs)
+  
 
 
-    def __str__(self) -> str:
-        return self.category_name
 
-
-class ColorVariant(BaseModel):
-    color_name = models.CharField(max_length=100)
-    price = models.IntegerField(default=0)
-
-    def __str__(self) -> str:
-        return self.color_name
-
-class SizeVariant(BaseModel):
-    size_name = models.CharField(max_length=100)
-    price = models.IntegerField(default=0)
-    
-    def __str__(self) -> str:
-        return self.size_name
 
 
 
@@ -42,18 +24,10 @@ class Product(BaseModel):
     category = models.ForeignKey(Category , on_delete=models.CASCADE , related_name="products")
     price = models.IntegerField()
     product_desription = models.TextField()
-    color_variant = models.ManyToManyField(ColorVariant , blank=True)
-    size_variant = models.ManyToManyField(SizeVariant , blank=True)
 
 
-    
-    def save(self , *args , **kwargs):
-        self.slug = slugify(self.product_name)
-        super(Product ,self).save(*args , **kwargs)
 
 
-    def __str__(self) -> str:
-        return self.product_name
 
 
 
